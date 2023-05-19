02166ee0704a13aa63c316ece3a5ba653c863bb0
mahlashrifi <mahla7997@gmail.com>
2023-05-19 23:51:00 +0330

    Change code to get variables from env vars

--- Part2/main.py
+++ Part2/main.py
@@ -5,37 +5,37 @@ import httpx
 import json
 import socket
 
+
 app = FastAPI()
 client = httpx.AsyncClient()
 
 r = redis.Redis(host='red', port=6379, decode_responses=True)
 
 
-@app.post("shorten")
+@app.post(os.getenv("END_POINT"))
 async def get_shorter_url(data: Request):
     data = await data.json()
 
     headers = {'accept': 'application/json',
-               'apikey': "My key",
+               'apikey': os.getenv("API_KEY"),
                'content-type': 'application/json'}
 
     apiUrl = 'https://api.rebrandly.com/v1/links'
-   
 
+   
     destination = data["destination"]
     is_cached = False
     short_url = None
 
     if (r.exists(destination) == 1):
         short_url = r.get(destination)
-        is_cached = True  
-
+        is_cached = True      
     else:
         json_data = json.dumps(data)
         response = await client.post(apiUrl, headers=headers, data=json_data)
         short_url = response.json()["shortUrl"]
         r.set(destination, short_url)
-        r.expire(destination, 300)
+        r.expire(destination, 60 * os.getenv("TIME_CACHED_VALID"))
     
   
     result = {}

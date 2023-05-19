d135751f54c4b3c0f082fba77bd18d4548a8dcb1
mahlashrifi <mahla7997@gmail.com>
2023-05-19 23:33:25 +0330

    Implement Simple server that is able to shorten links

--- Part2/main.py
+++ Part2/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI, Request
+import httpx
+import json
+
 app = FastAPI()
 
-@app.get("/")
-async def test():
-   return "I am working"
+@app.post("/shorten")
+async def get_shorter_url(data: Request):
+   data = await data.json()
+
+   # I did not push my key to github
+   headers = {'accept': 'application/json',
+             'apikey': "My Key",
+             'content-type': 'application/json'}
+   apiUrl = 'https://api.rebrandly.com/v1/links'
+
+   json_data = json.dumps(data)
+   response = await client.post(apiUrl, headers=headers, data=json_data)  
+
+   result = {}
+   result["longUrl"] = data["destination"]
+   result["shortUrl"] = response.json()["shortUrl"] 
+   result["isCached"] = False
+   result["hostname"] = "mahla"
+
+   return result

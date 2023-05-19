b123da6c4ebe3395ff46997e03b393b0b701df3b
mahlashrifi <mahla7997@gmail.com>
2023-05-19 23:44:52 +0330

    Add redis cache to server

--- Part2/main.py
+++ Part2/main.py
@@ -1,27 +1,48 @@
+import os
+import redis
 from fastapi import FastAPI, Request
 import httpx
 import json
+import socket
 
 app = FastAPI()
 client = httpx.AsyncClient()
 
-@app.post("/shorten")
+r = redis.Redis(host='red', port=6379, decode_responses=True)
+
+
+@app.post("shorten")
 async def get_shorter_url(data: Request):
-   data = await data.json()
+    data = await data.json()
 
-   # I did not push my key to github
-   headers = {'accept': 'application/json',
-             'apikey': "My Key",
-             'content-type': 'application/json'}
-   apiUrl = 'https://api.rebrandly.com/v1/links'
+    headers = {'accept': 'application/json',
+               'apikey': "My key",
+               'content-type': 'application/json'}
 
-   json_data = json.dumps(data)
-   response = await client.post(apiUrl, headers=headers, data=json_data)  
+    apiUrl = 'https://api.rebrandly.com/v1/links'
+   
 
-   result = {}
-   result["longUrl"] = data["destination"]
-   result["shortUrl"] = response.json()["shortUrl"] 
-   result["isCached"] = False
-   result["hostname"] = "mahla"
+    destination = data["destination"]
+    is_cached = False
+    short_url = None
 
-   return result
+    if (r.exists(destination) == 1):
+        short_url = r.get(destination)
+        is_cached = True  
+
+    else:
+        json_data = json.dumps(data)
+        response = await client.post(apiUrl, headers=headers, data=json_data)
+        short_url = response.json()["shortUrl"]
+        r.set(destination, short_url)
+        r.expire(destination, 300)
+    
+  
+    result = {}
+    result["longUrl"] = destination
+    result["shortUrl"] = short_url
+    result["isCached"] = is_cached
+    result["hostname"] = socket.gethostname()
+
+    return result
+

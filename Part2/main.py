7bb3f97d73cd4ebc291e1a9db4892c3fc2600094
mahlashrifi <mahla7997@gmail.com>
2023-05-22 22:33:10 +0330

    Make values configurable

--- Part2/main.py
+++ Part2/main.py
@@ -1,17 +1,15 @@
 import os
+
 import redis
 from fastapi import FastAPI, Request
 import httpx
 import json
 import socket
 
-
 app = FastAPI()
 client = httpx.AsyncClient()
 
-r = redis.Redis(host='red', port=6379, decode_responses=True)
-
-
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 @app.post(os.getenv("END_POINT"))
 async def get_shorter_url(data: Request):
     data = await data.json()
@@ -22,22 +20,20 @@ async def get_shorter_url(data: Request):
 
     apiUrl = 'https://api.rebrandly.com/v1/links'
 
-   
     destination = data["destination"]
     is_cached = False
     short_url = None
 
     if (r.exists(destination) == 1):
         short_url = r.get(destination)
-        is_cached = True      
+        is_cached = True
     else:
         json_data = json.dumps(data)
         response = await client.post(apiUrl, headers=headers, data=json_data)
         short_url = response.json()["shortUrl"]
         r.set(destination, short_url)
-        r.expire(destination, 60 * os.getenv("TIME_CACHED_VALID"))
-    
-  
+        r.expire(destination, os.getenv("VALID_CACHED_TIME") * 60)
+
     result = {}
     result["longUrl"] = destination
     result["shortUrl"] = short_url
@@ -46,3 +42,6 @@ async def get_shorter_url(data: Request):
 
     return result
 
+@app.get("/")
+async def get_shorter_url(data: Request): 
+    return socket.gethostname()
